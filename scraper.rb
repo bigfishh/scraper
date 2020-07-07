03e0193e34871157eb5811ae052050a35fe52a82
bigfishh <anniezheng.anniee@gmail.com>
2020-07-07 09:33:27 -0400

    exporting scraped data to CSV

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,18 @@
+require 'csv'
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
 
 html = open("https://en.wikipedia.org/wiki/Avatar:_The_Last_Airbender")
+doc = Nokogiri::HTML(html)
 
-response = Nokogiri::HTML(html)
+data_arr = []
 
-description = response.css("p").text.strip.split("\n")[0]
+description = doc.css("p").text.strip.split("\n")[0]
+picture = doc.css("td a img").find{|picture| picture.attributes["alt"].value.include?("Avatar The Last Airbender logo.svg")}.attributes["src"].value
 
-picture = response.css("td a img").find{|picture| picture.attributes["alt"].value.include?("Avatar The Last Airbender logo.svg")}.attributes["src"].value
+data_arr.push([description, picture])
 
+CSV.open('data.csv', "w") do |csv|
+    csv << data_arr
+end
 
-binding.pry 
-
-puts "hello"
