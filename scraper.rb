2625a8187ead0a946b3c6b9e665de1d3c86dde9c
bigfishh <anniezheng.anniee@gmail.com>
2020-07-07 09:18:41 -0400

    parsing HTML with nokogiri

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,16 @@
 require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
 html = open("https://en.wikipedia.org/wiki/Avatar:_The_Last_Airbender")
+
+response = Nokogiri::HTML(html)
+
+description = response.css("p").text.strip.split("\n")[0]
+
+picture = response.css("td a img").find{|picture| picture.attributes["alt"].value.include?("Avatar The Last Airbender logo.svg")}.attributes["src"].value
+
+
+binding.pry 
+
+puts "hello"
