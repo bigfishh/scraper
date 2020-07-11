df29e7b7120ef4d09660c19e4831531374b40588
bigfishh <anniezheng.anniee@gmail.com>
2020-07-10 23:08:14 -0400

    dynamic page scraping with Selenium and Headless Chrome

--- scraper.rb
+++ scraper.rb
@@ -21,54 +21,54 @@ end
 
 # # --------------------------- Static Page Scraping ---------------------------------------------------------------------->
 class JobScraper < Kimurai::Base
+    @name= 'eng_job_scraper'
+    @start_urls = ["https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY"]
+    @engine = :selenium_chrome
 
-  @name= 'eng_job_scraper'
-  @start_urls = ["https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY"]
-  @engine = :selenium_chrome
+    @@jobs = []
 
-  @@jobs = []
+    def scrape_page
+        doc = browser.current_response
+        returned_jobs = doc.css('td#resultsCol')
+        returned_jobs.css('div.jobsearch-SerpJobCard').each do |char_element|
+            title = char_element.css('h2 a')[0].attributes["title"].value.gsub(/\n/, "")
+            link = "https://indeed.com" + char_element.css('h2 a')[0].attributes["href"].value.gsub(/\n/, "")
+            description = char_element.css('div.summary').text.gsub(/\n/, "")
+            company = description = char_element.css('span.company').text.gsub(/\n/, "")
+            location = char_element.css('div.location').text.gsub(/\n/, "")
+            salary = char_element.css('div.salarySnippet').text.gsub(/\n/, "")
+            requirements = char_element.css('div.jobCardReqContainer').text.gsub(/\n/, "")
+            # job = [title, link, description, company, location, salary, requirements]
+            job = {title: title, link: link, description: description, company: company, location: location, salary: salary, requirements: requirements}
 
-  def scrape_page
-    doc = browser.current_response
-    returned_jobs = doc.css('td#resultsCol')
-    returned_jobs.css('div.jobsearch-SerpJobCard').each do |char_element|
-      # scraping individual listings 
-      title = char_element.css('h2 a')[0].attributes["title"].value.gsub(/\n/, "")
-      link = "https://indeed.com" + char_element.css('h2 a')[0].attributes["href"].value.gsub(/\n/, "")
-      description = char_element.css('div.summary').text.gsub(/\n/, "")
-      company = description = char_element.css('span.company').text.gsub(/\n/, "")
-      location = char_element.css('div.location').text.gsub(/\n/, "")
-      salary = char_element.css('div.salarySnippet').text.gsub(/\n/, "")
-      requirements = char_element.css('div.jobCardReqContainer').text.gsub(/\n/, "")
-
-      # creating a job object
-      job = {title: title, link: link, description: description, company: company, location: location, salary: salary, requirements: requirements}
-
-      # adding the object if it is unique
-      @@jobs << job if !@@jobs.include?(job)
+            @@jobs << job if !@@jobs.include?(job)
+        end  
     end
-  end
 
-  def parse(response, url:, data: {})
-     # scrape first page
-     scrape_page
+    def parse(response, url:, data: {})
 
-     # next page link starts with 20 so the counter will be initially set to 2
-     num = 2
- 
-     # visit next page and scrape it
-     10.times do
-         browser.visit("https://www.indeed.com/jobs?q=software+engineer&l=New+York,+NY&start=#{num}0")
-         scrape_page
-         num += 1
-     end
- 
-     @@jobs
+        10.times do
+            scrape_page
 
-     CSV.open('jobs.csv', "w") do |csv|
-        csv << @@jobs
+            if browser.current_response.css('div#popover-background') || browser.current_response.css('div#popover-input-locationtst')
+                browser.refresh 
+            end
+                    
+            browser.find('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/nav/div/ul/li[6]/a/span').click
+            puts "🔹 🔹 🔹 CURRENT NUMBER OF JOBS: #{@@jobs.count}🔹 🔹 🔹"
+            puts "🔺 🔺 🔺 🔺 🔺  CLICKED NEXT BUTTON 🔺 🔺 🔺 🔺 "
+        end
+
+        CSV.open('jobs.csv', "w") do |csv|
+            csv << @@jobs
+        end
+
+        File.open("jobs.json","w") do |f|
+            f.write(JSON.pretty_generate(@@jobs))
+        end
+        
+        @@jobs
     end
-    
-  end
-
 end
+
+jobs = JobScraper.crawl!
